793abfca81ad7eb103a2aa2ef313090eb162f206
Leitt0 <leandrobianchi3@gmail.com>
2022-04-20 12:41:23 -0300

    se agrego el addcategories y addvideogames

--- routes/category.js
+++ routes/category.js
@@ -2,11 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 const { list, create, remove, categoryById } = require('../controllers/categoryControllers');
+const { userById } = require('../controllers/authController')
 
 router.get('/categories', list);
-router.post('/create', create);
+router.post('/create/:userId', create);
 router.delete('/:categoryId', remove)
 
 router.param('categoryId', categoryById); 
+router.param('userId', userById)
 
 module.exports = router;
